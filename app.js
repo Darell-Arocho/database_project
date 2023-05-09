53eafc8c63be6d233376002cd28723a9532a77aa
HarryRQ <harry.riveraq@gmail.com>
2023-05-09 19:16:36 -0400

    Added comments to code

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
- const observer = new IntersectionObserver((entries) =>  {
+// Create a new IntersectionObserver object that takes in a callback function as a parameter.
+const observer = new IntersectionObserver((entries) => {
+    // Iterate through the entries and log each one to the console.
     entries.forEach((entry) => {
-        console.log(entry)
-        if (entry.isIntersecting) {
-            entry.target.classList.add('show');
-        } else {
-            entry.target.classList.remove('show');
-        }
+    console.log(entry);
+    // If the entry is intersecting, add the 'show' class to the target element.
+    if (entry.isIntersecting) {
+    entry.target.classList.add('show');
+    } else {
+    // If the entry is not intersecting, remove the 'show' class from the target element.
+    entry.target.classList.remove('show');
+    }
     });
- });
-
-const hiddenElements = document.querySelectorAll('.hidden');
-hiddenElements.forEach((el) => observer.observe(el));
+    });
+    
+    // Get all elements with the 'hidden' class and add the IntersectionObserver to each of them.
+    const hiddenElements = document.querySelectorAll('.hidden');
+    hiddenElements.forEach((el) => observer.observe(el));

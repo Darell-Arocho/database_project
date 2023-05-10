5f789412bc933d7ed7876ad72e46c313c33da4cb
Darell Arocho <darell.arocho@upr.edu>
2023-05-10 17:54:18 -0400

    Added back buttons to the remaining pages

--- sports.php
+++ sports.php
@@ -1,4 +1,25 @@
-<?php
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Sports</title>
+
+    <link rel="stylesheet" href="style.css" />
+    <script defer src="app.js"></script>
+
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css"
+      rel="stylesheet"
+    />
+  </head>
+  <body>
+    
+    <button onclick="location.href='index.html'">Go to home page</button>
+
+
+    <?php
 
     $serverName = "localhost";
     $userName = "darellal";
@@ -16,3 +37,5 @@
     echo "Connection successful!";
 
 ?>
+  </body>
+</html>

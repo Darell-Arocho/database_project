8289f0c4f61302f2e49b8c4dbe772cf6b84c3c3c
Darell Arocho <darell.arocho@upr.edu>
2023-05-11 15:11:50 -0400

    Changed title name for partidos entity

--- partidos.php
+++ partidos.php
@@ -9,7 +9,7 @@
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Historial</title>
+    <title>Partidos</title>
 
     <link rel="stylesheet" href="partidos_style.css" />
     <script defer src="app.js"></script>

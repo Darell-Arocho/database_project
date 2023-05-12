524bf5984649e036ed5e80f7580016b63f7c333b
Darell Arocho <darell.arocho@upr.edu>
2023-05-12 09:46:09 -0400

    Added functionality to modify logros y recs

---
+++ submit.inc.logros.php
@@ -0,0 +1,23 @@
+<?php
+    
+    // include_once 'dbh_inc.php';
+    
+    // if(isset($_POST['submit'])) {
+    //   $logroID = $_POST['logroID'];
+    //   $nombreLogro = $_POST['nombreLogro'];
+    //   $fecha = $_POST['fecha'];
+    //   $atletaID = $_POST['atletaID'];
+      
+    //   // Insert the data into the table
+    //   $sql = "INSERT INTO Logros_y_Reconocimientos (ID_Logro, Nombre_Logro, Fecha, AtletaID)
+    //           VALUES ('$logroID', '$nombreLogro', '$fecha', '$atletaID');";
+    //   mysqli_query($conn, $sql);
+      
+    //   // Redirect to the previous page
+    //   header("Location: index.html?submit=success");
+    //   exit();
+    // }
+  
+  
+      
+?>
